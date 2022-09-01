fdec197bf2e88985e6c96e7fc6ed98e953fea3a5
JonODonnell1 <60612952+JonODonnell1@users.noreply.github.com>
2022-09-01 11:10:17 -0400

    Added setup script

---
+++ setup.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+sudo apt-get -y install binfmt-support qemu qemu-user-static debootstrap kpartx \
+lvm2 dosfstools gpart binutils bison git lib32ncurses-dev libssl-dev gawk wget \
+diffstat unzip texinfo gcc-multilib build-essential chrpath socat \
+libsdl1.2-dev autoconf libtool libglib2.0-dev libarchive-dev xterm sed cvs \
+subversion kmod coreutils texi2html bc docbook-utils help2man \
+make gcc g++ desktop-file-utils libgl1-mesa-dev libglu1-mesa-dev mercurial \
+automake groff curl lzop asciidoc u-boot-tools mtd-utils device-tree-compiler \
+flex cmake python3-git python3-m2crypto udisks2
